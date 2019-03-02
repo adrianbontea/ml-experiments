9c253def70c0e487828a6341aa7774c217313693
Adrian Bontea <contact@adrianbontea.com>
2019-03-02 18:54:30 +0200

    New ExperimentRunner class getting concrete experiments via DI powered by dependency-injector framework

--- ML-Experiments/main.py
+++ ML-Experiments/main.py
@@ -1,9 +1,26 @@
 import asyncio
+from californiahousing.dataanalysis import DataAnalysisExperiment
 from californiahousing.preprocessing import PreProcessingExperiment
+import dependency_injector.containers as containers
+import dependency_injector.providers as providers
+
+class ExperimentRunner:
+    def __init__(self, experiment):
+        self.__experiment = experiment
+
+    async def run_async(self):
+        await self.__experiment.run_async()
+
+class ExperimentsContainer(containers.DeclarativeContainer):
+    data_analysis = providers.Factory(DataAnalysisExperiment)
+    preprocessing = providers.Factory(PreProcessingExperiment)
+
+class RunnersContainer(containers.DeclarativeContainer):
+    instance = providers.Factory(ExperimentRunner, experiment = ExperimentsContainer.data_analysis)
 
 async def main():
-    exp = PreProcessingExperiment()
-    await exp.run_async()
+    runner = RunnersContainer.instance()
+    await runner.run_async()
 
 asyncio.run(main())
 
