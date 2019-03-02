bd3428376aec72ffb0d44f9de8d812475e3cc660
Adrian Bontea <contact@adrianbontea.com>
2019-03-02 17:46:09 +0200

    Adjusted preprocessing example to use a pipeline of median inputer and min-max scaler and put the ocean_proximity one-hot vector back into the data frame as a list (of 5-length arrays)

--- ML-Experiments/californiahousing/preprocessing.py
+++ ML-Experiments/californiahousing/preprocessing.py
@@ -54,20 +54,26 @@ class PreProcessingExperiment(HousingExperimentBase):
         print(type(ocean_proximity_1_hot).__name__)
         print(ocean_proximity_1_hot.shape)
 
-        # Now this cannot fit anymore in a single column of the predictors_tr DataFrame as that needs to be a Pandas Series...
-
         # Transform the numeric predictors using a pipeline composed of an inputer and scaler
         pipeline = Pipeline([('imputer', SimpleImputer(strategy="median")),('scaler', MinMaxScaler())])
-        predicors_tr = pipeline.fit_transform(predictors_num)
+        predictors_tr = pipeline.fit_transform(predictors_num)
+
+        #predictors_tr is now a 2 dimensional nd array (16512, 8) - convert it back to a data frame
+        predictors_tr = pd.DataFrame(predictors_tr, columns=predictors_num.columns)
+
+        # ocean proximity one hot vector is now a 2 dimensional ndarray of shape (16512,5) - convert it to list in order to be able to save it as a data frame column
+        predictors_tr["ocean_proximity"] = ocean_proximity_1_hot.tolist()
+        print(predictors_tr.head())
 
         # Finally a full pipeline to transform both the numeric predictors and ocean_proximity into a single data set
-        num_attribs = list(predictors_num) # All column names from the numerical predictors DataFrame
-        cat_attribs = ["ocean_proximity"]
+        # Note: This doesn't work yet
+        #num_attribs = list(predictors_num) # All column names from the numerical predictors DataFrame
+        #cat_attribs = ["ocean_proximity"]
 
-        num_pipeline = Pipeline([('selector', DataFrameSelector(num_attribs)),('imputer', SimpleImputer(strategy="median")),('scaler', MinMaxScaler())])
-        cat_pipeline = Pipeline([('selector', DataFrameSelector(cat_attribs)),('encoder', LabelBinarizer())])
+        #num_pipeline = Pipeline([('selector', DataFrameSelector(num_attribs)),('imputer', SimpleImputer(strategy="median")),('scaler', MinMaxScaler())])
+        #cat_pipeline = Pipeline([('selector', DataFrameSelector(cat_attribs)),('encoder', LabelBinarizer())])
 
-        full_pipeline = FeatureUnion(transformer_list=[("num_pipeline", num_pipeline),("cat_pipeline", cat_pipeline)])
-        predictors_prepared = full_pipeline.fit_transform(predictors)
+        #full_pipeline = FeatureUnion(transformer_list=[("num_pipeline", num_pipeline),("cat_pipeline", cat_pipeline)])
+        #predictors_prepared = full_pipeline.fit_transform(predictors)
 
-        print(predictors_prepared.shape)
+        #print(predictors_prepared.shape)
