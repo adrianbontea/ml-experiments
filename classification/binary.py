988e9d0d4ffe45e769af5642da0bd4a820524469
Adrian Bontea <contact@adrianbontea.com>
2019-03-07 23:15:11 +0200

    Finished BinaryClassificationExperiment with Precision-Recall(Sensitivity) Tradeoff

--- classification/binary.py
+++ classification/binary.py
@@ -4,7 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, precision_recall_curve
 
 
 class BinaryClassificationExperiment(ClassificationExperimentBase):
@@ -73,6 +73,47 @@ class BinaryClassificationExperiment(ClassificationExperimentBase):
         print("f1 mean for random_state = 42:", f1_score(five_binary_labels, y_train_pred))
 
         # We get lower precision (more false positives) but higher sensitivity (less false negatives)
+        # Precision-Recall Tradeoff...
+        # Increasing the threshold increases precision and decreases recall
+        # Conversely, decreasing threshold increases recall and decreases precision
+
+        sgd_classifier = SGDClassifier(random_state=77)
+        sgd_classifier.fit(training_set_tr, five_binary_labels)
+
+        # Pick a five
+        index_of_five = self.__get_index_of_random_true(five_binary_labels)
+        some_digit = training_set_tr[index_of_five]
+
+        score = sgd_classifier.decision_function([some_digit])
+        threshold = 0
+
+        print(score > threshold)  # Outputs True when threshold is 0
+
+        threshold = 300000
+
+        print(score > threshold)  # Outputs False when increasing threshold
+
+        sgd_classifier = SGDClassifier(random_state=77)
+        # Get the scores of all instances in the training set using K-fold predictions
+        scores = cross_val_predict(sgd_classifier, training_set_tr, five_binary_labels, cv=3, method="decision_function")
+
+        # Now compute the Precision-Recall curves for each possible thresholds
+        precisions, recalls, thresholds = precision_recall_curve(five_binary_labels, scores)
+        # Get list of thresholds that would result in a precision of at least 88% and recall of at least 70%
+        # Note ndarrays of precision and recall (dimension 1) are one element larger than thresholds
+        # so we select all but last element
+
+        thresholds_for_precision_and_recall = self.__get_threholds_for(precisions[:-1], recalls[:-1], thresholds, 0.88, 0.70)
+        print("Thresholds for Precision at least 0.88 and Recall at least 0.7 are:", thresholds_for_precision_and_recall)
+
+        # We get 33 possible threshold values for which the precision would be at least 88% and recall at least 70%
+        # Test one of them
+        test_threshold = thresholds_for_precision_and_recall[np.random.randint(0, len(thresholds_for_precision_and_recall) - 1)]
+        predictions = scores > test_threshold
+
+        print("Precision:", precision_score(five_binary_labels, predictions))
+        print("Sensitivity", recall_score(five_binary_labels, predictions))
+
 
     def __get_index_of_random_true(self, binary_labels):
         indexes_of_trues = np.where(binary_labels == True)[0]
@@ -81,3 +122,12 @@ class BinaryClassificationExperiment(ClassificationExperimentBase):
     def __get_index_of_random_false(self, binary_labels):
         indexes_of_falses = np.where(binary_labels == False)[0]
         return indexes_of_falses[np.random.randint(0, len(indexes_of_falses) - 1)]
+
+    def __get_threholds_for(self, precisions, recalls, thresholds, precision_at_least, recall_at_least):
+        indexes_precisions = np.where(precisions > precision_at_least)[0]
+        indexes_recalls = np.where(recalls > recall_at_least)[0]
+        intersection_indexes = np.intersect1d(indexes_precisions, indexes_recalls)
+
+        return thresholds[intersection_indexes]
+
+
