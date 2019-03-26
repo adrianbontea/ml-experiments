b69bfcab7c5c6c7d84498aee2fad3397a292dfcb
Adrian Bontea <contact@adrianbontea.com>
2019-03-26 21:14:56 +0200

    Added more experiments to linear and non-linear SVM classification and decision region plotting via mlxtend

--- svm/linearsvmclassification.py
+++ svm/linearsvmclassification.py
@@ -2,28 +2,39 @@ from base import ExperimentBase
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from mlxtend.plotting import plot_decision_regions
 
 
 class LinearSvmClassificationExperiment(ExperimentBase):
     async def do_run_async(self):
         iris = load_iris()
+
+        # Training with just 2 features
         X = iris.data[:, 2:4]  # Petal length, Petal Width - Shape (150,2)
 
         # Binary classification
-        y = (iris.target == 2)  # Iris-Virginica
-
-        # Plot Original Data Set - Virginica and Non-Virginica instances
-        colors = ['green', 'blue']
-        plt.scatter(X[:, 0], X[:, 1], c=y, cmap= ListedColormap(colors))
-        plt.show()
+        y = (iris.target == 2).astype(np.int)  # Iris-Virginica
 
         scaler = StandardScaler()
         X_tr = scaler.fit_transform(X)
 
+        # Plot Scaled Data Set with binary labels - Virginica and Non-Virginica instances
+        colors = ['green', 'blue']
+        plt.scatter(X_tr[:, 0], X_tr[:, 1], c=y, cmap=ListedColormap(colors))
+        plt.show()
+
         svm_classifier = LinearSVC(C=1, loss="hinge")
         svm_classifier.fit(X_tr, y)
+
+        # Plot decision regions for binary labels
+        plot_decision_regions(X_tr, y, svm_classifier)
+        plt.xlabel('Petal Length [cm]')
+        plt.ylabel('Petal Width [cm]')
+        plt.title('SVM on Iris - 2 Features - Binary Classes')
+        plt.show()
 
         print("A flower with petal length = 5.5 and petal width = 1.7 is Iris-Virginica?", svm_classifier.predict([[5.5, 1.7]]))
 
@@ -31,7 +42,32 @@ class LinearSvmClassificationExperiment(ExperimentBase):
         y = iris.target
         X_tr = scaler.fit_transform(X)
 
+        # Plot Scaled Data Set with multiclass
+        colors = ['green', 'blue', 'red', 'yellow']
+        plt.scatter(X_tr[:, 0], X_tr[:, 1], c=y, cmap=ListedColormap(colors))
+        plt.show()
+
         svm_classifier = LinearSVC(C=1, loss="hinge")
         svm_classifier.fit(X_tr, y)
 
+        # Plot decision regions for multiclass
+        plot_decision_regions(X_tr, y, svm_classifier)
+        plt.xlabel('Petal Length [cm]')
+        plt.ylabel('Petal Width [cm]')
+        plt.title('SVM on Iris - 2 Features - Multi Classes')
+        plt.show()
+
         print("A flower with petal length = 5.5 and petal width = 1.7 is:", iris.target_names[svm_classifier.predict([[5.5, 1.7]])[0]])
+
+        # Training with all 4 features
+        X = iris.data  # Sepal Length, Sepal Width, Petal length, Petal Width - Shape (150,4)
+
+        # Binary classification
+        y = (iris.target == 2).astype(np.int)  # Iris-Virginica
+        X_tr = scaler.fit_transform(X)
+
+        svm_classifier = LinearSVC(C=1, loss="hinge")
+        svm_classifier.fit(X_tr, y)
+        print("A flower with sepal length = 1.3, sepal width = 2.2, petal length = 5.5 and petal width = 1.7 is Iris-Virginica?", svm_classifier.predict([[1.3, 2.2, 5.5, 1.7]]))
+
+        # Plot decision regions for binary labels when multiple features?
--- svm/nonlinearsvmclassification.py
+++ svm/nonlinearsvmclassification.py
@@ -1,11 +1,14 @@
 from base import ExperimentBase
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.pipeline import Pipeline
 from sklearn.datasets import make_moons
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from mlxtend.plotting import plot_decision_regions
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 
 class NonLinearSvmClassificationExperiment(ExperimentBase):
@@ -32,4 +35,62 @@ class NonLinearSvmClassificationExperiment(ExperimentBase):
 
         random_index = np.random.randint(0, 100)
         print("Non-linear SVM prediction:", classifier.predict([X_tr[random_index, ]]))
+        print("Label:", y[random_index])
+
+        # Adding polynomial features is simple to implement and can work great with all sorts
+        # of Machine Learning algorithms (not just SVMs), but at a low polynomial degree it
+        # cannot deal with very complex datasets, and with a high polynomial degree it creates
+        # a huge number of features, making the model too slow.
+
+        # Using Polynomial Kernel
+        # It makes it possible to
+        # get the same result as if you added many polynomial features, even with very high degree
+        # polynomials, without actually having to add them. So there is no combinatorial
+        # explosion of the number of features since you don’t actually add any features. This
+        # trick is implemented by the SVC class
+
+        scaler = StandardScaler()
+        X_tr = scaler.fit_transform(X)
+
+        classifier = SVC(kernel="poly", degree=3, coef0=1, C=5)
+        classifier.fit(X_tr, y)
+
+        # Plot decision regions
+        plot_decision_regions(X_tr, y, classifier)
+        plt.title('3 Degree Polynomial Kernel SVM Classifier on Moons')
+        plt.show()
+
+        classifier = SVC(kernel="poly", degree=10, coef0=1, C=5)
+        classifier.fit(X_tr, y)
+
+        # Plot decision regions
+        plot_decision_regions(X_tr, y, classifier)
+        plt.title('10 Degree Polynomial Kernel SVM Classifier on Moons')
+        plt.show()
+
+        # Let's try other classifiers without the polynomial features (just the original 2) so we can plot
+        # their decision regions and compare
+
+        # Try Random Forest Classifier and decision regions
+        classifier = RandomForestClassifier()
+        classifier.fit(X_tr, y)
+
+        # Plot decision regions
+        plot_decision_regions(X_tr, y, classifier)
+        plt.title('Random Forest Classifier on Moons')
+        plt.show()
+
+        print("Random Forest prediction:", classifier.predict([X_tr[random_index, ]]))
+        print("Label:", y[random_index])
+
+        # Try K-Nearest Neighbours Classifier and decision regions
+        classifier = KNeighborsClassifier()
+        classifier.fit(X_tr, y)
+
+        # Plot decision regions
+        plot_decision_regions(X_tr, y, classifier)
+        plt.title('K-Nearest Neighbours Classifier on Moons')
+        plt.show()
+
+        print("K-Nearest Neighbours prediction:", classifier.predict([X_tr[random_index, ]]))
         print("Label:", y[random_index])
