b772205c4be1867f5cae2336a99116cff295bf86
Adrian Bontea <contact@adrianbontea.com>
2019-05-02 21:58:02 +0300

    Adjusted main module to be able to pass the experiment to run as a command line argument

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from decisiontrees import *
 from ensemble import *
 import dependency_injector.containers as containers
 import dependency_injector.providers as providers
+import sys
 
 
 class ExperimentRunner:
@@ -18,32 +19,40 @@ class ExperimentRunner:
 
 
 class ExperimentsContainer(containers.DeclarativeContainer):
-    data_analysis = providers.Factory(DataAnalysisExperiment)
-    preprocessing = providers.Factory(PreProcessingExperiment)
-    models = providers.Factory(ModelsExperiment)
-    binary_classification = providers.Factory(BinaryClassificationExperiment)
-    multiclass_classification = providers.Factory(MulticlassClassificationExperiment)
-    multilabel_classification = providers.Factory(MultilabelClassificationExperiment)
-    multioutput_classification = providers.Factory(MultioutputClassificationExperiment)
-    whitebox_linear_regression = providers.Factory(LinearRegressionExperiment)
-    whitebox_polynomial_regression = providers.Factory(PolynomialRegressionExperiment)
-    whitebox_logistic_regression = providers.Factory(LogisticRegressionExperiment)
-    svm_linear_classification = providers.Factory(LinearSvmClassificationExperiment)
-    svm_non_linear_classification = providers.Factory(NonLinearSvmClassificationExperiment)
-    svm_linear_regression = providers.Factory(LinearSvmRegressionExperiment)
-    svm_non_linear_regression = providers.Factory(NonLinearSvmRegressionExperiment)
-    decision_tree_visualization = providers.Factory(DecisionTreeVisualizationExperiment)
-    decision_tree_regularization = providers.Factory(DecisionTreeRegularizationExperiment)
-    decision_tree_regression = providers.Factory(DecisionTreeRegressionExperiment)
-    voting_classifier = providers.Factory(VotingClassifierExperiment)
-    bagging_pasting = providers.Factory(BaggingAndPastingExperiment)
-    random_forest = providers.Factory(RandomForestExperiment)
-    ada_boosting = providers.Factory(AdaBoostingExperiment)
-    stacking = providers.Factory(StackingExperiment)
+    experiments = {
+        "data_analysis": providers.Factory(DataAnalysisExperiment),
+        "preprocessing": providers.Factory(PreProcessingExperiment),
+        "models": providers.Factory(ModelsExperiment),
+        "binary_classification": providers.Factory(BinaryClassificationExperiment),
+        "multiclass_classification": providers.Factory(MulticlassClassificationExperiment),
+        "multilabel_classification": providers.Factory(MultilabelClassificationExperiment),
+        "multioutput_classification": providers.Factory(MultioutputClassificationExperiment),
+        "whitebox_linear_regression": providers.Factory(LinearRegressionExperiment),
+        "whitebox_polynomial_regression": providers.Factory(PolynomialRegressionExperiment),
+        "whitebox_logistic_regression": providers.Factory(LogisticRegressionExperiment),
+        "svm_linear_classification": providers.Factory(LinearSvmClassificationExperiment),
+        "svm_non_linear_classification": providers.Factory(NonLinearSvmClassificationExperiment),
+        "svm_linear_regression": providers.Factory(LinearSvmRegressionExperiment),
+        "svm_non_linear_regression": providers.Factory(NonLinearSvmRegressionExperiment),
+        "decision_tree_visualization": providers.Factory(DecisionTreeVisualizationExperiment),
+        "decision_tree_regularization": providers.Factory(DecisionTreeRegularizationExperiment),
+        "decision_tree_regression": providers.Factory(DecisionTreeRegressionExperiment),
+        "voting_classifier": providers.Factory(VotingClassifierExperiment),
+        "bagging_pasting": providers.Factory(BaggingAndPastingExperiment),
+        "random_forest": providers.Factory(RandomForestExperiment),
+        "ada_boosting": providers.Factory(AdaBoostingExperiment),
+        "stacking": providers.Factory(StackingExperiment)
+    }
+    @staticmethod
+    def get_experiment():
+        if len(sys.argv) > 1 and sys.argv[1] in ExperimentsContainer.experiments:
+            return ExperimentsContainer.experiments[sys.argv[1]]
+        else:
+            return ExperimentsContainer.experiments["data_analysis"]
 
 
 class RunnersContainer(containers.DeclarativeContainer):
-    instance = providers.Factory(ExperimentRunner, experiment=ExperimentsContainer.stacking)
+    instance = providers.Factory(ExperimentRunner, experiment=ExperimentsContainer.get_experiment())
 
 
 async def main():
